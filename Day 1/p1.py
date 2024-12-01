282efd67f66acc8f109cab3ac74faa080e7f08a8
evanliou <evanliou@yahoo.com>
2024-12-01 01:15:09 -0800

    Day 1

--- Day 1/p1.py
+++ Day 1/p1.py
@@ -5,4 +5,18 @@ os.chdir("Day 1")
 with open("input.txt") as f:
     lines = f.readlines()
 
+total = 0
+nums1 = []
+nums2 = []
 
+for line in lines:
+    temp = [int(x) for x in line.split()]
+    nums1.append(temp[0])
+    nums2.append(temp[1])
+
+nums1.sort()
+nums2.sort()
+
+for i in range(len(nums1)):
+    total += abs(nums1[i] - nums2[i])
+print(total)
--- Day 1/p2.py
+++ Day 1/p2.py
@@ -5,3 +5,15 @@ os.chdir("Day 1")
 with open("input.txt") as f:
     lines = f.readlines()
 
+total = 0
+nums1 = []
+nums2 = {}
+
+for line in lines:
+    temp = [int(x) for x in line.split()]
+    nums1.append(temp[0])
+    nums2[temp[1]] = 1 + nums2.get(temp[1], 0)
+
+for i in nums1:
+    total += i * nums2.get(i, 0)
+print(total)
