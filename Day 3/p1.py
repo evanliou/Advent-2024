544f9aaf2aa6f562838df7078f106c0135ff1cbb
evanliou <evanliou@yahoo.com>
2024-12-02 21:37:15 -0800

    Day 3

--- Day 3/p1.py
+++ Day 3/p1.py
@@ -1,7 +1,19 @@
 import os
+import re
 
 os.chdir("Day 3")
 
 with open("input.txt") as f:
     lines = f.readlines()
 
+line = "".join(lines)
+
+multOps = re.findall(r"mul\(\d+\,\d+\)", line)
+
+total = 0
+
+for x in multOps:
+    a = int(x[1 + x.index("(") : x.index(",")])
+    b = int(x[1 + x.index(",") : x.index(")")])
+    total += a * b
+print(total)
--- Day 3/p2.py
+++ Day 3/p2.py
@@ -1,7 +1,26 @@
 import os
+import re
 
 os.chdir("Day 3")
 
 with open("input.txt") as f:
     lines = f.readlines()
 
+line = "".join(lines)
+
+ops = re.findall(r"mul\(\d+\,\d+\)|do\(\)|don\'t\(\)", line)
+
+enabled = True
+
+total = 0
+for x in ops:
+    if x == "do()":
+        enabled = True
+    elif x == "don't()":
+        enabled = False
+    else:
+        if enabled:
+            a = int(x[1 + x.index("(") : x.index(",")])
+            b = int(x[1 + x.index(",") : x.index(")")])
+            total += a * b
+print(total)
